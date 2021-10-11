be096d1b79d48bdf42dbb8faf11029f307cf3d6c
Jamie1919 <jayprice883@gmail.com>
2021-10-11 21:41:02 +0200

    Ch4V13

--- Java Advanced Project 4/Methods/src/com/company/Main.java
+++ Java Advanced Project 4/Methods/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
