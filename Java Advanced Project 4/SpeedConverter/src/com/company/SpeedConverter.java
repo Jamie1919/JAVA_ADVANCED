fd29910c23dcf0f456f4465da33a72fbbf957304
Jamie1919 <jayprice883@gmail.com>
2021-10-12 14:58:52 +0200

    Ch4V13

--- Java Advanced Project 4/SpeedConverter/src/com/company/SpeedConverter.java
+++ Java Advanced Project 4/SpeedConverter/src/com/company/SpeedConverter.java
@@ -4,6 +4,7 @@ public class SpeedConverter {
 
     public static long toMilesPerHour(double kilometersPerHour) {
         if(kilometersPerHour < 0) {
+            
             return -1;
         }
 
