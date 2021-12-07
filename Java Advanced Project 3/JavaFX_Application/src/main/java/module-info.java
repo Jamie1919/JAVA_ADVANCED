896ce237e943aa34dcff68075057f7e5df637b7f
Jamie1919 <jayprice883@gmail.com>
2021-12-07 10:08:37 +0200

    Ch13V35

--- Java Advanced Project 3/JavaFX_Application/src/main/java/module-info.java
+++ Java Advanced Project 3/JavaFX_Application/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javafx_application {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
     opens com.example.javafx_application to javafx.fxml;
